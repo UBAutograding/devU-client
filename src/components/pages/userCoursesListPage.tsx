9ad8900f3e3324d3b82273586ecce2c95eee2a1d
Michael Drewitt <mikedrewitt@gmail.com>
2021-07-25 16:29:17 -0400

    * Added dropdown as filter
    * TODO
      * Store filter changes into local storage and read from ls for default
      * Touchup css (make it prettier)
      * Use actual loading overlay component
      * Use actual error component

--- src/components/pages/userCoursesListPage.tsx
+++ src/components/pages/userCoursesListPage.tsx
@@ -3,18 +3,30 @@ import { UserCourse, Course } from 'devu-shared-modules'
 
 import PageWrapper from 'components/shared/layouts/pageWrapper'
 import UserCourseListItem from 'components/listItems/userCourseListItem'
+import Dropdown from 'components/shared/inputs/dropdown'
 
 import RequestService from 'services/request.service'
+
+import styles from './userCoursesListPage.scss'
+
+const filterOptions = [
+  { label: 'All', value: 'all' },
+  { label: 'Active', value: 'active' },
+  { label: 'Inactive', value: 'inactive' },
+  { label: 'Dropped', value: 'dropped' },
+]
 
 const UserCoursesListPage = () => {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
   const [userCourses, setUserCourses] = useState(new Array<UserCourse>())
   const [courses, setCourses] = useState<Record<string, Course>>({})
+  const [filter, setFilter] = useState<'all' | 'active' | 'inactive' | 'dropped'>('active')
 
   const fetchData = async () => {
     try {
-      const userCourses: UserCourse[] = await RequestService.get(`/api/user-courses`)
+      // The filter isn't implemented by the API yet
+      const userCourses: UserCourse[] = await RequestService.get(`/api/user-courses?filterBy=${filter}`)
       const courseRequests = userCourses.map((u) => RequestService.get(`/api/courses/${u.courseId}`))
       const courses: Course[] = await Promise.all(courseRequests)
 
@@ -33,15 +45,26 @@ const UserCoursesListPage = () => {
 
   useEffect(() => {
     fetchData()
-  }, [])
+  }, [filter])
 
   if (loading) return <div>Loading</div>
   if (error) return <div>{error}</div>
 
+  const defaultOption = filterOptions.find((o) => o.value === filter)
+
   return (
     <PageWrapper>
+      <div className={styles.filters}>
+        <Dropdown
+          label='Filter Courses'
+          className={styles.dropdown}
+          options={filterOptions}
+          onChange={setFilter}
+          defaultOption={defaultOption}
+        />
+      </div>
       {userCourses.map((userCourse) => (
-        <UserCourseListItem userCourse={userCourse} course={courses[userCourse.id || '']} />
+        <UserCourseListItem key={userCourse.courseId} userCourse={userCourse} course={courses[userCourse.id || '']} />
       ))}
     </PageWrapper>
   )
