18acd9e7f76769f14df0a15dd60748465ee706b2
beetosu <cpwalsh@buffalo.edu>
2021-08-05 12:20:02 -0400

    Establish basic API connection

--- src/components/pages/courseDetailPage.tsx
+++ src/components/pages/courseDetailPage.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Course } from 'devu-shared-modules'
 
 import PageWrapper from 'components/shared/layouts/pageWrapper'
+import LoadingOverlay from 'components/shared/loaders/loadingOverlay'
 
-const CourseDetailPage = ({}) => <PageWrapper>Course Detail</PageWrapper>
+import RequestService from 'services/request.service'
+import { useParams } from 'react-router-dom'
+import ErrorPage from './errorPage'
 
+type Params = {
+    courseId: string
+}
+
+const CourseDetailPage = () => {
+    const [loading, setLoading] = useState(true)
+    const [course, setCourse] = useState({} as Course)
+    const [error, setError] = useState(null)
+
+    const { courseId } = useParams() as Params
+
+    useEffect(() => {
+        RequestService.get(`/api/courses/${courseId}`)
+            .then(setCourse)
+            .catch(setError)
+            .finally(() => setLoading(false))
+    }, [])
+
+    if (loading) return <LoadingOverlay />
+    if (error) return <ErrorPage error={error} />
+
+    return (
+        <PageWrapper>
+            <div>{course.name}</div>
+        </PageWrapper>
+    )
+}
 export default CourseDetailPage
