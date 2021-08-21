9e70b065a8fd5ad4aec6eddf9b4581799b9eb6f0
beetosu <cpwalsh@buffalo.edu>
2021-08-21 15:56:35 -0400

    Add in needed UI/Sass

--- src/components/pages/courseDetailPage.tsx
+++ src/components/pages/courseDetailPage.tsx
@@ -1,44 +1,76 @@
 import React, { useEffect, useState } from 'react'
-import { Assignment, Course } from 'devu-shared-modules'
+import { Assignment, Course, UserCourse } from 'devu-shared-modules'
 
 import PageWrapper from 'components/shared/layouts/pageWrapper'
 import LoadingOverlay from 'components/shared/loaders/loadingOverlay'
+import Dropdown, { Option } from 'components/shared/inputs/dropdown'
+import Button from 'components/shared/inputs/button'
+import AssignmentListItem from 'components/listItems/assignmentListItem'
+
+import ErrorPage from './errorPage'
+import styles from './courseDetailPage.scss'
 
 import RequestService from 'services/request.service'
-import { useParams } from 'react-router-dom'
-import ErrorPage from './errorPage'
-import AssignmentListItem from 'components/listItems/assignmentListItem'
+import LocalStorageService from 'services/localStorage.service'
+import { Link, useParams } from 'react-router-dom'
+import { prettyPrintDate } from 'utils/date.utils'
+import { useAppSelector } from 'redux/hooks'
+
+const ORDER_BY_STORAGE_KEY = 'course_assignments_order_by'
+const GROUP_BY_STORAGE_KEY = 'course_assignments_group_by'
+
+type OrderBy = 'activeDueDate' | 'dueDate' | 'active' | 'assignmentName'
+type GroupBy = 'id' | 'category' | 'name' | 'dueDate' | 'active'
+
+const orderByOptions: Option<OrderBy>[] = [
+  { label: 'Default', value: 'activeDueDate' },
+  { label: 'Due Date', value: 'dueDate' },
+  { label: 'Active', value: 'active' },
+  { label: 'Name', value: 'assignmentName' },
+]
+
+const groupByOptions: Option<GroupBy>[] = [
+    { label: 'Defualt', value: 'id' },
+    { label: 'Category', value: 'category' },
+    { label: 'Name', value: 'name' },
+    { label: 'Due Date', value: 'dueDate' },
+    { label: 'Active', value: 'active' }
+]
 
 type Params = {
     courseId: string
 }
 
 const CourseDetailPage = () => {
+    const defaultOrderBy = LocalStorageService.get<OrderBy>(ORDER_BY_STORAGE_KEY) || 'activeDueDate'
+    const defaultGroupBy = LocalStorageService.get<GroupBy>(GROUP_BY_STORAGE_KEY) || 'id'
+
     const [loading, setLoading] = useState(true);
     const [course, setCourse] = useState({} as Course);
+    const [userCourse, setUserCourse] = useState<UserCourse>({} as UserCourse);
     const [assignments, setAssignments] = useState(new Array<Assignment>());
     const [error, setError] = useState(null);
-    const [isActive, setActive] = useState(false);
+    const [orderBy, setOrderBy] = useState<OrderBy>(defaultOrderBy);
+    const [groupBy, setGroupBy] = useState<GroupBy>(defaultGroupBy);
 
     const { courseId } = useParams() as Params;
 
     useEffect(() => {
-        fetchCourse();
+        fetchCourse(orderBy, groupBy);
     }, []);
 
-    const fetchCourse = async () => {
+    const userId = useAppSelector((store) => store.user.id);
+
+    const fetchCourse = async (orderBy: OrderBy, groupBy: GroupBy) => {
         try {
             const pageCourse = await RequestService.get(`/api/courses/${courseId}`);
-            /*
-                Assuming active/inactive is relative to the users' current date,
-                determine whether a course is active based on the current date's 
-                relation to the start/end time.
-            */
-            if (new Date(pageCourse.startDate) < new Date() && new Date(pageCourse.endDate) > new Date()) {
-                setActive(true);
-            }
             setCourse(pageCourse);
-            setAssignments(await RequestService.get(`/api/assignments`));
+            setAssignments(await RequestService.get(`/api/assignments?orderBy=${orderBy}?groupBy=${groupBy}`));
+
+            // Find the users relationship to the course by calling all user-course objects, and seeing if any have the user/course id.
+            const userCourses = await RequestService.get<UserCourse[]>(`/api/user-courses`);
+            const uCourse = userCourses.filter(u => u.courseId === parseInt(courseId) && u.userId === userId).shift()
+            if (uCourse) { setUserCourse(uCourse); }
         }
         catch(error) {
             setError(error)
@@ -47,20 +79,66 @@ const CourseDetailPage = () => {
         }
     }
 
-    if (loading) return <LoadingOverlay />
+    const handleFilterChange = (updatedOrderBy: OrderBy) => {
+        setOrderBy(updatedOrderBy)
+        fetchCourse(updatedOrderBy, groupBy)
+    
+        LocalStorageService.set(ORDER_BY_STORAGE_KEY, updatedOrderBy)
+      }
+    
+      const handleGroupByChange = (updatedGroupBy: GroupBy) => {
+        setGroupBy(updatedGroupBy)
+        fetchCourse(orderBy, updatedGroupBy)
+    
+        LocalStorageService.set(GROUP_BY_STORAGE_KEY, updatedGroupBy)
+      }
+
+    if (loading) return <LoadingOverlay delay={250} />
     if (error) return <ErrorPage error={error} />
+
+    const defaultOrderByOption = orderByOptions.find((o) => o.value === orderBy)
+    const defaultGroupByOption = groupByOptions.find((o) => o.value === groupBy)
 
     return (
         <PageWrapper>
-            <div className="header">
-                <h2 className="name">{course.name}</h2>
-                <h3 className="subtitle">{isActive ? 'Active' : 'Inactive'} | {course.semester}</h3>
-            </div>
-            <div className="assignments">
-                <h2>Assignments</h2>
-            {assignments.map((assignment) => (
-                <AssignmentListItem key={assignment.id} assignment={assignment} courseId={course.id?.toString() || ''} />
-            ))}
+            <header>
+                <h1>{course.name}</h1>
+                <div className={styles.subtitle}>
+                    <h3>{course.number} | {course.semester} ({prettyPrintDate(course.startDate)}-{prettyPrintDate(course.endDate)})</h3>
+                    {userCourse.level === "instructor" &&
+                        <Link to={`${courseId}/update`}><Button>Update Course</Button></Link>
+                    } 
+                </div>
+            </header>
+            {userCourse &&
+                <div>
+                    <Link className={styles.button} to={`${courseId}/assignments`}><Button>Gradebook</Button></Link>
+                    <Link className={styles.button} to={`${courseId}/roster`}><Button>Roster</Button></Link>
+                </div>
+            }
+            <div>
+                <div className={styles.assignmentsHeader}>
+                    <h2>Assignments</h2>
+                    <div className={styles.dropdowns}>
+                        <Dropdown
+                            label='Group By'
+                            className={styles.dropdown}
+                            options={groupByOptions}
+                            onChange={handleGroupByChange}
+                            defaultOption={defaultGroupByOption}
+                        />
+                        <Dropdown
+                            label='Order By'
+                            className={styles.dropdown}
+                            options={orderByOptions}
+                            onChange={handleFilterChange}
+                            defaultOption={defaultOrderByOption}
+                        />
+                    </div>
+                </div>
+                {assignments.map((assignment) => (
+                    <AssignmentListItem key={assignment.id} assignment={assignment} courseId={course.id?.toString() || ''} />
+                ))}
             </div>
         </PageWrapper>
     )
